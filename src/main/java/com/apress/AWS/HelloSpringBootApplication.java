ac1422445df7c640681cb56ae51c7f1a0a227f54
Tran Quoc Tuan <tuantran1295@MacBook-Air-cua-Tran.local>
2024-11-15 10:45:40 +0700

    initial commit

--- src/main/java/com/apress/AWS/HelloSpringBootApplication.java
+++ src/main/java/com/apress/AWS/HelloSpringBootApplication.java
@@ -2,15 +2,27 @@ package com.apress.AWS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class HelloSpringBootApplication {
+public class HelloSpringBootApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(HelloSpringBootApplication.class);
+	}
+	  
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringBootApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String helloMessage() {
+		return "Hello Spring Boot!";
 	}
 	
 	@RequestMapping("/greeting")
